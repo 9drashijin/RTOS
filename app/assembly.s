5cd053e49eafc0a1812e407b5d665ad3b5209f0f
9drashijin <9drashijin@live.com>
2015-06-22 12:21:21 +0800

    added hash
    Signed-off-by: 9drashijin <9drashijin@live.com>

--- app/assembly.s
+++ app/assembly.s
@@ -11,7 +11,7 @@ testFunc:
  movs    r0, #0x12
  movs    r1, #0x34
  movs    r2, #0x56
- ldr	 r3, =0x123456
+ ldr	 r3, =#0x123456 //same as movw and movt
  movw.w  r4, 0x123
  movt.w  r4, 0x456
  bx  	 lr
